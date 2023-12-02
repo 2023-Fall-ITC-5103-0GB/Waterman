b24b41b8685580d9cfdb0e91d14c56d7e4cb7bfb
Rafiat <bolajirafiat@gmail.com>
2023-12-01 19:27:20 -0500

    Worked on JavaScript for my dealer page

---
+++ RafiatJavaScript/Contact.js
@@ -0,0 +1,11 @@
+        function subscription() {
+        let name1 = document.getElementById('email1').value;
+        console.log('Thanks for subscribing!' + name1);
+        document.getElementById('email1').value = '';
+        
+        let message = 'Thanks for subscribing!';
+        alert (message);
+      }
+      
+      
+    //   subscription();
---
+++ RafiatJavaScript/Dealers.js
@@ -0,0 +1,48 @@
+
+let myImgArray = [
+    'images/dealer.png',
+    'images/dealer3.jpg',
+    'images/dealer4.jpg',
+    'images/dealer5.jpg',
+  ];
+  
+  // Section 2 -------------------------------------------------
+  //make an automatic slide to display the images
+  let ImgNumber = 0;
+  let difference = myImgArray.length - 1;
+  
+  let delay = 3000; //milliseconds    1sec=1000milliseconds
+  
+  setInterval('ChangeImages(1)', delay);
+  //-1 to show the slide backwards
+  //1 to show the slide forwards
+  
+  function ChangeImages(direction) {
+    //begin function
+  
+    ImgNumber = ImgNumber + direction;
+  
+    if (ImgNumber > difference) {
+      //begin inner first if
+      ImgNumber = 0;
+    } //end inner first if
+  
+    if (ImgNumber < 0) {
+      //begin inner second if
+      ImgNumber = difference;
+    } //end inner second if
+    //document.slideshow.src = myImagesArray[ImageNumber];
+    document.getElementById('deliver').src = myImgArray[ImgNumber];
+  } //end function
+  
+let slideInterval = setInterval(nextSlide, 5000);
+
+function pause() {
+    clearInterval(slideInterval);
+}
+
+function resume() {
+    slideInterval = setInterval(nextSlide, 5000);
+  }
+
+  
